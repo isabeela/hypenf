965a6c390bcd066d70a36835bf56dca8e31fe516
isabeela <31148780+isabeela@users.noreply.github.com>
2024-05-09 14:31:06 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,23 +1,25 @@
-$(document).ready(function() {
-  $('form').on('submit', function(e) {
+
+document.addEventListener('DOMContentLoaded', function() {
+  document.querySelector('form').addEventListener('submit', function(e) {
     e.preventDefault();
 
     var formData = new FormData(this);
 
-    $.ajax({
-      type: 'POST',
-      url: 'https://af9618c4-a602-4633-ade9-8b7152b7c1a1-00-od1vbydpfhbj.riker.replit.dev/', // Substitua pelo URL do seu servidor no Replit
-      data: formData,
-      processData: false,
-      contentType: false,
-      success: function(response) {
-        alert(response); // Exibe a mensagem de sucesso
-        $('form')[0].reset(); // Limpa o formulário
-      },
-      error: function(xhr, status, error) {
+    var xhr = new XMLHttpRequest();
+    xhr.open('POST', 'https://af9618c4-a602-4633-ade9-8b7152b7c1a1-00-od1vbydpfhbj.riker.replit.dev/upload'); // Substitua pelo URL do seu servidor no Replit
+    xhr.onload = function() {
+      if (xhr.status === 200) {
+        alert(xhr.responseText); // Exibe a mensagem de sucesso
+        document.querySelector('form').reset(); // Limpa o formulário
+      } else {
         alert('Erro ao enviar arquivo para o Google Drive.');
-        console.error(error);
+        console.error(xhr.statusText);
       }
-    });
+    };
+    xhr.onerror = function() {
+      alert('Erro ao enviar arquivo para o Google Drive.');
+      console.error(xhr.statusText);
+    };
+    xhr.send(formData);
   });
 });
