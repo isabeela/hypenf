b4477e19d6e91aa62e51718f449e9f6d2e0bc19c
isabeela <31148780+isabeela@users.noreply.github.com>
2024-07-19 15:52:54 -0300

    ajuste js

--- script.js
+++ script.js
@@ -2,50 +2,33 @@ document.addEventListener('DOMContentLoaded', function() {
   document.querySelector('form').addEventListener('submit', function(e) {
     e.preventDefault();
 
-      var nome = document.getElementById('nome').value;
-      var email = document.getElementById('email').value;
-      var salario = document.getElementById('salario').value;
-      const checkboxes = document.querySelectorAll('input[type="checkbox"][name="projeto"]:checked');
-      const projetos = Array.from(checkboxes).map(checkbox => checkbox.value);
+    var nome = document.getElementById('nome').value;
+    var email = document.getElementById('email').value;
+    var salario = document.getElementById('salario').value;
+    const checkboxes = document.querySelectorAll('input[type="checkbox"][name="projeto"]:checked');
+    const projetos = Array.from(checkboxes).map(checkbox => checkbox.value);
 
-      if (projetos.length === 0) {
-        alert('Selecione pelo menos um projeto.');
-        return;
-      }
+    if (projetos.length === 0) {
+      alert('Selecione pelo menos um projeto.');
+      return;
+    }
 
-      var formData = new FormData(this);
-      formData.append('salario', salario);
-      formData.append('projetos', projetos.join(','));
+    var formData = new FormData(this);
+    formData.append('salario', salario);
+    formData.append('projetos', projetos.join(','));
 
-      fetch('https://hype-nf-hypeadm.replit.app/upload', {
-        method: 'POST',
-        body: formData,
-      })
-      .then(response => response.text())
-      .then(result => {
-        alert(result);
-      })
-      .catch(error => {
-        console.error('Erro:', error);
-
-      });
- 
-
-    var xhr = new XMLHttpRequest();
-    xhr.open('POST', 'https://hype-nf-hypeadm.replit.app/upload'); // Substitua pelo URL do seu servidor no Replit
-    xhr.onload = function() {
-      if (xhr.status === 200) {
-        alert(xhr.responseText); // Exibe a mensagem de sucesso
-        document.querySelector('form').reset(); // Limpa o formulário
-      } else {
-        alert('Erro ao enviar arquivo para o Google Drive.');
-        console.error(xhr.statusText);
-      }
-    };
-    xhr.onerror = function() {
+    fetch('https://hype-nf-hypeadm.replit.app/upload', {
+      method: 'POST',
+      body: formData,
+    })
+    .then(response => response.text())
+    .then(result => {
+      alert(result);
+      document.querySelector('form').reset(); // Limpa o formulário
+    })
+    .catch(error => {
+      console.error('Erro:', error);
       alert('Erro ao enviar arquivo para o Google Drive.');
-      console.error(xhr.statusText);
-    };
-    xhr.send(formData);
+    });
   });
 });
